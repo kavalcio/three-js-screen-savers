aaeafd9b4c04cfd78554445306f37d4181ce76ab
kavalcio <sarpkavalcioglu@gmail.com>
2022-12-11 00:36:18 -0500

    Try deployment

---
+++ webpack.dev.js
@@ -0,0 +1,16 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+        directory: path.join(__dirname, ''),
+    },
+    liveReload: true,
+    compress: true,
+    port: 3010,
+  },
+});
