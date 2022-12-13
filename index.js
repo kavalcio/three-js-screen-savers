520a0f7598005baa541eee521fbf9c92f65ddfdc
kavalcio <sarpkavalcioglu@gmail.com>
2022-12-12 21:18:45 -0500

    Add ui

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { Vector3 } from 'three';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GUI } from 'dat.gui';
 
 const PIPE_LENGTH_MAX = 15;
 const PIPE_LENGTH_MIN = 5;
@@ -18,6 +19,14 @@ const BOUNDS = {
     min: { x: -130, y: -70, z: -140 },
     max: { x: 130, y: 70, z: 60},
 };
+
+const PARAMS = {
+    maxPipeLength: PIPE_LENGTH_MAX,
+    minPipeLength: PIPE_LENGTH_MIN,
+    buildSpeed: PIPE_BUILD_SPEED,
+}
+
+const pipeList = [];
 
 // TODO: prevent pipe from overlapping itself
 // TODO: add ui and parameterize pipe length, pipe build speed, etc
@@ -49,6 +58,12 @@ function init() {
     const controls = new OrbitControls(camera, renderer.domElement);
     document.body.appendChild(renderer.domElement);
 
+    // Create GUI
+    const gui = new GUI();
+    gui.add(PARAMS, 'maxPipeLength', 8, 100);
+    gui.add(PARAMS, 'minPipeLength', 1, 7);
+    gui.add(PARAMS, 'buildSpeed', 0, 20);
+
     // Create lights
     const ambientLight = new THREE.AmbientLight(0x404040);
     scene.add(ambientLight);
@@ -57,9 +72,6 @@ function init() {
     directionalLight.position.x = 1;
     directionalLight.position.z = 1;
     scene.add(directionalLight);
-
-    // Create pipes
-    const pipeList = [];
 
     const getRandomColor = () => {
         return "#" + Math.floor(Math.random()*16777215).toString(16);
@@ -79,7 +91,7 @@ function init() {
         if (parentMesh) {
             mesh = parentMesh.clone();
         } else {
-            const geometry = new THREE.CylinderGeometry(PIPE_RADIUS, PIPE_RADIUS, 1, 32);
+            const geometry = new THREE.CylinderGeometry(PARAMS.pipeRadius, PARAMS.pipeRadius, 1, 32);
             const material = new THREE.MeshPhongMaterial({ color: getRandomColor() });
             mesh = new THREE.Mesh(geometry, material);
         }
@@ -101,7 +113,7 @@ function init() {
 
     function getRandomEndpoint({ directions, oldEnd }) {
         const randomDirection = directions[getRandomInt(0, directions.length - 1)];
-        const randomLength = getRandomInt(PIPE_LENGTH_MIN, PIPE_LENGTH_MAX);
+        const randomLength = getRandomInt(PARAMS.minPipeLength, PARAMS.maxPipeLength);
         const newEnd = oldEnd.clone().addScaledVector(randomDirection, randomLength);
 
         if (
@@ -130,13 +142,13 @@ function init() {
             const direction = pipe.end.clone().sub(pipe.start);
             if (direction.length() > pipe.currentScale) {
                 // Translate
-                pipe.mesh.position.addScaledVector(direction.clone().normalize(), PIPE_BUILD_SPEED / 2);
+                pipe.mesh.position.addScaledVector(direction.clone().normalize(), PARAMS.buildSpeed / 2);
                 if (pipe.mesh.position.clone().sub(pipe.start).length() > direction.length() / 2) {
                     pipe.mesh.position.copy(pipe.start).addScaledVector(direction, 0.5);
                 }
 
                 // Scale
-                pipe.currentScale += PIPE_BUILD_SPEED;
+                pipe.currentScale += PARAMS.buildSpeed;
                 if (pipe.currentScale > direction.length()) {
                     pipe.currentScale = direction.length();
                 }
@@ -146,7 +158,7 @@ function init() {
                 pipeList.splice(index, 1);
 
                 // Create sphere at corner
-                const geometry = new THREE.SphereGeometry(PIPE_RADIUS, 32, 32);
+                const geometry = new THREE.SphereGeometry(PARAMS.pipeRadius, 32, 32);
                 const material = new THREE.MeshPhongMaterial({ color: pipe.mesh.material.color.getHex() });
                 const sphere = new THREE.Mesh(geometry, material);
                 sphere.position.copy(pipe.end);
