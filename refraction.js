95f910ee3b1d7d76c6ed9ae850f2963ef922e57e
kavalcio <sarpkavalcioglu@gmail.com>
2023-01-07 23:05:43 -0500

    Update import path

--- refraction.js
+++ refraction.js
@@ -70,7 +70,7 @@ function init() {
   scene.add(sphereMesh);
 
   // Create background
-  const bgTexture = new THREE.TextureLoader().load('assets/xp_background.jpg');
+  const bgTexture = new THREE.TextureLoader().load('asset/xp_background.jpg');
   const bgGeometry = new THREE.PlaneGeometry(100, 60);
   const bgMaterial = new THREE.MeshBasicMaterial({ map: bgTexture });
   const bgMesh = new THREE.Mesh(bgGeometry, bgMaterial);
