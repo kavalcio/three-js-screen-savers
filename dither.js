c2751c75289daeb69a9baeae40a2bde87f5432d8
kavalcio <sarpkavalcioglu@gmail.com>
2023-06-12 10:02:03 +0300

    Get bayer dither partially working

--- dither.js
+++ dither.js
@@ -15,6 +15,7 @@ import { ShaderPass } from 'three/addons/postprocessing/ShaderPass.js';
 import { DotScreenShader } from 'three/examples/jsm/shaders/DotScreenShader.js';
 import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from './shaders/fragment.glsl'
+import bgImage from './asset/xp_background.jpg';
 // import { DotScreenShader } from './jsm/shaders/DotScreenShader.js';
 // import { EffectComposer } from './jsm/postprocessing/EffectComposer.js';
 // import { RenderPass } from './jsm/postprocessing/RenderPass.js';
@@ -26,6 +27,22 @@ let params = {
   angle: 0,
 };
 
+// https://codegolf.stackexchange.com/questions/259633/make-a-custom-bayer-matrix
+const getNormalizedBayerMatrix = (n) => {
+  // const matrix = [];
+  // for (let i = 0; i < size; i++) {
+  //   matrix[i] = [];
+  //   for (let j = 0; j < size; j++) {
+  //     matrix[i][j] = 0;
+  //   }
+  // }
+  // return matrix;
+  let g;
+  const matrix = [...Array(1<<n)].map((_,y,a) => a.map(g=(k=n,x)=>k--&&4*g(k,x)|2*(x>>k)+3*(y>>k&1)&3));
+  return matrix;
+};
+// const getNormalizedBayerMatrix = n=>[...Array(1<<n)].map((_,y,a)=>a.map(g=(k=n,x)=>k--&&4*g(k,x)|2*(x>>k)+3*(y>>k&1)&3));
+
 function init() {
   // Create scene
   const scene = new THREE.Scene();
@@ -33,6 +50,8 @@ function init() {
   // Create camera
   const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 1000);
   camera.position.z = 100;
+  // const camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 1, 1000 );
+  // camera.position.z = 10;
   const tanFOV = Math.tan(((Math.PI / 180) * camera.fov / 2));
   const initialWindowHeight = window.innerHeight;
 
@@ -74,6 +93,7 @@ function init() {
   
     // Update material
     material.uniforms.uTime.value = elapsedTime;
+    bgMaterial.uniforms.uTime.value = elapsedTime;
   };
 
   const material = new THREE.RawShaderMaterial({
@@ -87,7 +107,7 @@ function init() {
   console.log('vertexShader', vertexShader)
   console.log('fragmentShader', fragmentShader)
   const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+  // scene.add(cube);
 
   const geometry2 = new THREE.IcosahedronGeometry(10);
   const material2 = new THREE.MeshPhongMaterial({ color: 0x004499 });
@@ -95,7 +115,38 @@ function init() {
   cube2.position.x = 10;
   cube2.position.y = 10;
   cube2.position.z = -20;
-  scene.add(cube2);
+  // scene.add(cube2);
+
+  // TODO: use bayer matrix, function here
+  // const bayerMatrix = getNormalizedBayerMatrix(3);
+  const bayerMatrix = getNormalizedBayerMatrix(2);
+  console.log('bayerMatrix', bayerMatrix)
+
+  // Create background
+  const bgTexture = new THREE.TextureLoader().load(bgImage);
+  // const bgMaterial = new THREE.RawShaderMaterial({
+  const bgMaterial = new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader,
+    transparent: true,
+    // map: bgTexture,
+    uniforms: {
+      uTime: { value: 0.0 },
+      uMap: { type: 't', value: bgTexture },
+      uThresholdMap: { value: bayerMatrix },
+      // uThresholdMap: new THREE.Uniform(bayerMatrix),
+    },
+  });
+  const bgGeometry = new THREE.PlaneGeometry(100, 60);
+  const bgMesh = new THREE.Mesh(bgGeometry, bgMaterial);
+  scene.add(bgMesh);
+
+  // const geometry3 = new THREE.IcosahedronGeometry(10);
+  // const cube3 = new THREE.Mesh(geometry3, bgMaterial);
+  // cube2.position.x = 10;
+  // cube2.position.y = 10;
+  // cube2.position.z = -20;
+  // scene.add(cube3);
 
   // Create GUI
   const gui = new GUI();
--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,8 +1,40 @@
+#define USE_MAP true;
 precision mediump float;
 
 uniform float uTime;
+varying vec2 vUv;
+// uniform sampler2D texture1;
+uniform sampler2D uMap;
+uniform mat4 uThresholdMap;
+
+float random(vec2 uv)
+{
+  return fract(sin(dot(uv, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
+
+mat4 M = mat4(
+  vec4(0.0 / 16.0, 8.0 / 16.0, 2.0 / 16.0, 10.0 / 16.0),
+  vec4(12.0 / 16.0, 4.0 / 16.0, 14.0 / 16.0, 6.0 / 16.0),
+  vec4(3.0 / 16.0, 11.0 / 16.0, 1.0 / 16.0, 9.0 / 16.0),
+  vec4(15.0 / 16.0, 7.0 / 16.0, 13.0 / 16.0, 5.0 / 16.0)
+);
+
 
 void main()
 {
-  gl_FragColor = vec4(sin(uTime / 2.) + 0.2, cos(uTime * 2.) + 0.2, 0.0, 0.5);
+  vec4 color = texture2D(uMap, vUv);
+  // float brightness = sqrt(pow(color.r, 2.) + pow(color.g, 2.) + pow(color.b, 2.));
+  float brightness = (0.2126 * color.r + 0.7152 * color.g + 0.0722 * color.b);
+ 
+  // float thresholdValue = 0.5;
+  // float thresholdValue = uThresholdMap[int(mod(gl_FragCoord.x, 4.))][int(mod(gl_FragCoord.y, 4.))];
+  float thresholdValue = M[int(mod(gl_FragCoord.x, 4.))][int(mod(gl_FragCoord.y, 4.))];
+
+  // color = (brightness + random(vUv) - 0.5) > 0.5 ? vec4(0.8, 0.8, 0.8, 1.) : vec4(0.1, 0.15, 0.1, 1.);
+  color = (brightness) > thresholdValue ? vec4(0.85, 0.9, 0.85, 1.) : vec4(0.1, 0.15, 0.1, 1.);
+
+  gl_FragColor = color;
+  // gl_FragColor = vec4(floor(mod(gl_FragCoord.x / 16., 8.)) / 8., 0., 0., 1.);
+  // gl_FragColor = vec4(1., 0., 0., 1.);
 }
