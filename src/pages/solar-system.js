39ff5f75fb78487429d7479880e26d196e6eca2a
kavalcio <sarpkavalcioglu@gmail.com>
2024-01-30 23:27:35 -0500

    Remove unused imports

--- src/pages/solar-system.js
+++ src/pages/solar-system.js
@@ -1,8 +1,4 @@
 import * as THREE from 'three';
-import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
-import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
-import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
-import { ShaderPass } from 'three/addons/postprocessing/ShaderPass.js';
 
 import { initializeScene } from '/src/pages/template';
 import {
@@ -13,6 +9,7 @@ import {
 } from '../utils/solar-system';
 
 // TODO: try out tonemapping
+// TODO: get outer wilds assets using AssetStudio
 
 const {
   scene,
